46d94c18fea68ac1b8987f63c1e6efcaef73564f
OliveOil1 <47705687+OliveOil1@users.noreply.github.com>
2021-11-23 20:53:45 +1300

    Add project files.

---
+++ Battlelog Desktop/BrowserWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Battlelog_Desktop
+{
+	/// <summary>
+	/// Interaction logic for BrowserWindow.xaml
+	/// </summary>
+	/// 
+
+	public partial class BrowserWindow : Window
+	{
+		public string gameId = "gameId";
+
+		public BrowserWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void BrowserView_Loaded(object sender, RoutedEventArgs e)
+		{
+			BrowserView.DefaultBackgroundColor = System.Drawing.Color.Transparent;
+			BrowserView.Source = new Uri($"https://accounts.ea.com/connect/auth?locale=en_US&state={gameId}&redirect_uri=https%3A%2F%2Fbattlelog.battlefield.com%2Fsso%2F%3Ftokentype%3Dcode&response_type=code&client_id=battlelog&display=web%2Flogin");
+		}
+
+		private void BrowserView_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+		{
+			BackgroundImage.ImageSource = null;
+			Title = BrowserView.CoreWebView2.DocumentTitle;
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			BackgroundImage.ImageSource = new BitmapImage(new Uri($"pack://Application:,,,/Images/Backgrounds/{gameId}_background.jpg"));
+		}
+	}
+}
---
+++ Battlelog Desktop/Launcher.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Battlelog_Desktop
+{
+	/// <summary>
+	/// Interaction logic for Launcher.xaml
+	/// </summary>
+	public partial class Launcher : Window
+	{
+		public Launcher()
+		{
+			InitializeComponent();
+		}
+
+		private void GameButton_Click(object sender, RoutedEventArgs e)
+		{
+			string gameId = "bf4";
+			
+			if (sender == BF3Button)
+			{
+				gameId = "bf3";
+			}
+			else if (sender == BF4Button)
+			{
+				gameId = "bf4";
+			}
+			else if (sender == BFHButton)
+			{
+				gameId = "bfh";
+			}
+			else if (sender == MOHWButton)
+			{
+				gameId = "mohw";
+			}
+
+			StartGame(gameId);
+		}
+
+		private void CloseWindowButton_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void MinimizeWindowButton_Click(object sender, RoutedEventArgs e)
+		{
+			WindowState = WindowState.Minimized;
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			if(App.Args.Length == 2 && App.Args[0] == "-game")
+			{
+				if(App.Args[1] == "bf3" || App.Args[1] == "bf4" || App.Args[1] == "bfh" || App.Args[1] == "mohw")
+				{
+					StartGame(App.Args[1]);
+				}
+			}
+		}
+
+		private void StartGame(string gameId)
+		{
+			var window = new BrowserWindow();
+			window.gameId = gameId;
+			window.Show();
+			Close();
+		}
+	}
+}
